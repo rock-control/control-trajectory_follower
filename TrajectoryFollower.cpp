f7d322a207a84eea2eaa6d37a6ad55b21f7037ad
Janosch Machowinski <janosch.machowinski@dfki.de>
2012-06-22 14:21:27 +0200

    moved trajectory follower from orogen module into own class

---
+++ TrajectoryFollower.cpp
@@ -0,0 +1,145 @@
+#include "TrajectoryFollower.hpp"
+
+namespace trajectory_follower {
+
+
+TrajectoryFollower::TrajectoryFollower(double forwardLength, double gpsCenterofRotationOffset, int controllerType):
+    bInitStable(false), newTrajectory(true), hasTrajectory(false), status(REACHED_TRAJECTORY_END), forwardLength(forwardLength), gpsCenterofRotationOffset(gpsCenterofRotationOffset), controllerType(controllerType)
+{
+    if(controllerType != 0 && 
+	controllerType != 1 && 
+	controllerType != 2)
+	throw std::runtime_error("Wrong controller type given (not 0, 1 or 2)");
+}
+
+void TrajectoryFollower::setNewTrajectory(const base::Trajectory &trajectory)
+{
+    currentTrajectory = trajectory;
+    currentTrajectory.spline.setGeometricResolution(0.001);
+    bInitStable = false;
+    newTrajectory = true;
+    hasTrajectory = true;
+}
+    
+void TrajectoryFollower::removeTrajectory()
+{
+    hasTrajectory = false;
+}
+    
+double angleLimit(double angle)
+{
+    if(angle > M_PI)
+	return angle - 2*M_PI;
+    else if (angle < -M_PI)
+	return angle + 2*M_PI;
+    else
+     	return angle;
+}
+    
+enum TrajectoryFollower::FOLLOWER_STATUS TrajectoryFollower::traverseTrajectory(Eigen::Vector2d &motionCmd, const base::Pose &robotPose)
+{   
+    motionCmd(0) = 0.0; 
+    motionCmd(1) = 0.0; 
+
+    if(!hasTrajectory)
+	return REACHED_TRAJECTORY_END;
+
+    base::Trajectory &trajectory(currentTrajectory);
+
+    if(newTrajectory)
+    {
+	newTrajectory = false;
+        para =  trajectory.spline.findOneClosestPoint(robotPose.position, 0.001);
+    }    
+
+    pose.position = robotPose.position;
+    pose.heading  = robotPose.getYaw();
+    
+    if ( para < trajectory.spline.getEndParam() )
+    {
+
+        if(controllerType == 0)
+        {
+            pose.position.x() = pose.position.x() - (forwardLength + gpsCenterofRotationOffset) * sin(pose.heading);
+            pose.position.y() = pose.position.y() + (forwardLength + gpsCenterofRotationOffset) * cos(pose.heading);
+        }
+        else
+        {
+            pose.position.x() = pose.position.x() - (gpsCenterofRotationOffset) * sin(pose.heading);
+            pose.position.y() = pose.position.y() + (gpsCenterofRotationOffset) * cos(pose.heading);
+        }
+
+        Eigen::Vector3d vError = trajectory.spline.poseError(pose.position, pose.heading, para);
+        para  = vError(2);
+       
+	error.d = vError(0);
+        error.theta_e = angleLimit(vError(1) + M_PI_2);
+        error.param = vError(2);
+        
+        curvePoint.pose.position 	= trajectory.spline.getPoint(para); 	    
+        curvePoint.pose.heading  	= trajectory.spline.getHeading(para);
+        curvePoint.param 		= para;
+
+	//disable this test for testing, as it seems to be not needed
+	bInitStable = true;
+        if(!bInitStable)
+        {
+	    switch(controllerType)
+	    {
+		case 0:
+		    bInitStable = oTrajController_nO.checkInitialStability(error.d, error.theta_e, trajectory.spline.getCurvatureMax());
+		    bInitStable = true;
+		    break;
+		case 1:
+		    bInitStable = oTrajController_P.checkInitialStability(error.d, error.theta_e, trajectory.spline.getCurvature(para), trajectory.spline.getCurvatureMax());
+		    break;
+		case 2:
+		    bInitStable = oTrajController_PI.checkInitialStability(error.d, error.theta_e, trajectory.spline.getCurvature(para), trajectory.spline.getCurvatureMax());
+		    break;
+		default:
+		    throw std::runtime_error("Got bad controllerType value");
+	    }
+
+            if (!bInitStable)
+            {
+                std::cout << "Trajectory controller: failed initial stability test";
+                return INITIAL_STABILITY_FAILED;
+            }
+        }
+
+        double vel = currentTrajectory.speed;
+	switch(controllerType)
+	{
+	    case 0:
+		motionCmd = oTrajController_nO.update(vel, error.d, error.theta_e); 
+		break;
+	    case 1:
+		motionCmd = oTrajController_P.update(vel, error.d, error.theta_e, trajectory.spline.getCurvature(para), trajectory.spline.getVariationOfCurvature(para));
+		break;
+	    case 2:
+		motionCmd = oTrajController_PI.update(vel, error.d, error.theta_e, trajectory.spline.getCurvature(para), trajectory.spline.getVariationOfCurvature(para));
+		break;
+	    default:
+		throw std::runtime_error("Got bad controllerType value");
+	}
+    }
+    else
+    {
+	if(status != REACHED_TRAJECTORY_END)
+	{
+	    std::cout << "Reached end of trajectory" << std::endl;
+	    status = REACHED_TRAJECTORY_END;
+	}
+	return REACHED_TRAJECTORY_END;
+    }
+
+    if(status != RUNNING)
+    {
+	std::cout << "Started to follow trajectory" << std::endl;
+	status = RUNNING;
+    }
+
+    return RUNNING;    
+}
+
+}
---
+++ TrajectoryFollower.hpp
@@ -0,0 +1,99 @@
+#ifndef TRAJECTORYFOLLOWER_HPP
+#define TRAJECTORYFOLLOWER_HPP
+
+#include <base/trajectory.h>
+#include <base/motion_command.h>
+#include <base/pose.h>
+
+#include "TrajectoryControllerNoOrientation.hpp"
+#include "TrajectoryControllerP.hpp"
+#include "TrajectoryControllerPI.hpp"
+#include "TrajectoryFollowerTypes.hpp"
+
+namespace trajectory_follower {
+
+class TrajectoryFollower
+{
+
+public:
+    enum FOLLOWER_STATUS
+    {
+	RUNNING,
+	REACHED_TRAJECTORY_END,
+	INITIAL_STABILITY_FAILED,
+    };
+
+    TrajectoryFollower(double forwardLength, double gpsCenterofRotationOffset, int controllerType);
+    
+    /**
+     * Sets a new trajectory
+     * **/
+    void setNewTrajectory(const base::Trajectory &trajectory);
+
+    /**
+     * Marks the current trajectory as traversed
+     * */
+    void removeTrajectory();
+    
+    /**
+     * Gerenrates motion commands that should make the robot follow the
+     * trajectory
+     * */
+    enum FOLLOWER_STATUS traverseTrajectory(Eigen::Vector2d &motionCmd, const base::Pose &robotPose);
+
+    const TrajError &getControlError() const
+    {
+	return error;
+    }
+
+    const CurvePoint &getCurvePoint() const
+    {
+	return curvePoint;
+    }
+    
+    const RobotPose & getPose() const
+    {
+	return pose;
+    }
+    
+    trajectory_follower::noOrientation &getNoOrientationController()
+    {
+	return oTrajController_nO;
+    }
+
+    trajectory_follower::chainedProportional &getPController()
+    {
+	return oTrajController_P;
+    }
+
+    trajectory_follower::chainedProportionalIntegral &getPIController()
+    {
+	return oTrajController_PI;
+    }
+    
+private:
+    bool bInitStable;
+    bool newTrajectory;
+    bool hasTrajectory;
+    base::Trajectory currentTrajectory;
+
+    FOLLOWER_STATUS status;
+    
+    double forwardLength;
+    double gpsCenterofRotationOffset;
+    int controllerType;
+    
+    double para;
+    
+    trajectory_follower::noOrientation oTrajController_nO;
+    trajectory_follower::chainedProportional oTrajController_P;
+    trajectory_follower::chainedProportionalIntegral oTrajController_PI;
+    
+    //only class members for debug reasons
+    TrajError error;
+    CurvePoint curvePoint;
+    RobotPose pose;    
+};
+
+}
+#endif // TRAJECTORYFOLLOWER_HPP
